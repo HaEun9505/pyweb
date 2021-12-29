ce15b5dddfaa1fb0eadf2f37bec0ec3026cc2e30
HaEun9505 <0104whgkdms@naver.com>
2021-12-29 12:22:24 +0900

    댓글 수정, 삭제

--- board/views.py
+++ board/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
-from board.models import Question, Answer
+from board.models import Question, Answer, Comment
 from board.forms import QuestionForm, AnswerForm, CommentForm
 
 def index(request):
@@ -136,3 +136,30 @@ def comment_create_question(request, question_id):
         form = CommentForm()
     context = {'form':form}
     return render(request, 'board/comment_form.html', context)
+
+@login_required(login_url='common:login')
+def comment_delete_question(request, comment_id):
+    #질문 댓글 삭제
+    comment = get_object_or_404(Comment, pk=comment_id)
+    comment.delete()
+    return redirect('board:detail', question_id=comment.question.id)
+
+@login_required(login_url='common:login')
+def comment_modify_question(request, comment_id):
+    # 질문 댓글 수정
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)  #변경된 입력 내용
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.modify_date = timezone.now()    #수정일
+            comment.save()
+            return redirect('board:detail', question_id=comment.question.id)
+
+    else:
+        form = CommentForm(instance=comment)    #채워진 폼
+    context = {'form':form}
+    return render(request, 'board/comment_form.html', context)
+
+
